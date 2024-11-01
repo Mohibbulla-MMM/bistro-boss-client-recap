76a9f73c669d3e83758dd0b61ddde6de2b16683f
mohibbulla mohibbulla <rockychain1020@gmail.com>
2024-11-01 21:22:05 +0600

    menu json file added

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,11 +1,14 @@
- 
-
-const SectionTitle = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const SectionTitle = ({ heading, subHeading }) => {
+  return (
+    <div className="text-center w-full pt-10">
+      <p className="text-[#D99904] italic mb-3">---{heading}---</p>
+      <div>
+        <h1 className=" px-12 py-3 text-4xl md:text-5xl font-semibold text-black border-y-4 border-gray-300 uppercase inline-block select-none">
+          {subHeading}
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default SectionTitle;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Carosel from "../Carosel/Carosel";
 import Category from "../Category/Category";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 const Home = () => {
   return (
     <div>
       <Carosel />
       <Category />
+      <SectionTitle heading={" asf asdf465as "} subHeading={"mohibbulah"} />
     </div>
   );
 };
